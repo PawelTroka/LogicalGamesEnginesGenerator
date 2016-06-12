a8d5d843f0a9038fac76a586f846ae6f1b13d151
PTroka <pawel.troka@outlook.com>
2016-06-13 01:38:39 +0200

    Stockfish analysis started in master thesis - introduction, bitboard introduction and asm processor instructions done.

--- Connect(m,n,k,p,q)EnginesGenerator/(m,n,k,p,q)GameEngine/(m,n,k,p,q)GameEngine.cpp
+++ Connect(m,n,k,p,q)EnginesGenerator/(m,n,k,p,q)GameEngine/(m,n,k,p,q)GameEngine.cpp
@@ -2,6 +2,18 @@
 //
 
 #include "stdafx.h"
+/*
+#define M {_m_placeholder_}
+#define N {_n_placeholder_}
+#define K {_k_placeholder_}
+#define P {_p_placeholder_}
+#define Q {_q_placeholder_}
+*/
+
+class Board
+{
+	unsigned char pieces[M*N];
+};
 
 extern "C"
 {//http://stackoverflow.com/questions/10109590/unable-to-find-an-entry-point-when-calling-c-dll-in-c-sharp/10109659#10109659
